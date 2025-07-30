0274f2a51a97f3c6e5edbc06c7c7fc33c786f11a
Simon <omojide.o.j@gmail.com>
2025-07-30 06:56:03 +0100

    Refactor eventLoader

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -3,14 +3,19 @@ import { createBrowserRouter, RouterProvider } from 'react-router';
 
 import RootLayer from './routes/Root';
 import HomePage from './routes/Home';
-import EventPage, { Loader as EventLoader } from './routes/event/EventPage';
+import EventPage from './routes/event/EventPage';
 import EventDetailPage from './routes/event/EventDetailPage';
-import EventDetailLoader, {EventDetailAction as DeletEventAction } from './loader-action/EventDetail';
+import EventLoader from './loader-action/EventLoader';
+import EventDetailLoader, {
+	EventDetailAction as DeletEventAction,
+} from './loader-action/EventDetail';
 import NewEventPage from './routes/event/NewEventPage';
 import EditEventPage from './routes/event/EditEventPage';
 import EventRootLayer from './routes/event/EventRoot';
 import ErrorPage from './routes/ErrorPage';
-import newEventAction  from './loader-action/EventFormAction';
+import newEventAction from './loader-action/EventFormAction';
+import NewsletterPage from './components/NewsLetter';
+import newsLetterAction from './loader-action/NewLetterAction';
 
 const router = createBrowserRouter([
 	{
@@ -33,13 +38,26 @@ const router = createBrowserRouter([
 						id: 'event-detail',
 						loader: EventDetailLoader,
 						children: [
-							{ index: true, element: <EventDetailPage />, action: DeletEventAction },
-							{ path: 'edit', element: <EditEventPage />, action: newEventAction },
+							{
+								index: true,
+								element: <EventDetailPage />,
+								action: DeletEventAction,
+							},
+							{
+								path: 'edit',
+								element: <EditEventPage />,
+								action: newEventAction,
+							},
 						],
 					},
 
 					{ path: 'new', element: <NewEventPage />, action: newEventAction },
 				],
+			},
+			{
+				path: 'newsletter',
+				element: <NewsletterPage />,
+				action: newsLetterAction,
 			},
 		],
 	},
@@ -54,5 +72,3 @@ const App = () => {
 };
 
 export default App;
-
-// BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
---
+++ FRONTEND/src/loader-action/EventLoader.js
@@ -0,0 +1,15 @@
+
+const EventLoader = async () => {
+	const response = await fetch('http://localhost:8080/events');
+
+	if (!response.ok) {
+		throw new Response(JSON.stringify({ message: 'Could not fetch events' }), {
+			status: 500,
+		});
+	} else {
+		return response;
+	}
+};
+
+
+export default EventLoader;
